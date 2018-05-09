108f4e80382f6d88bf92c35e92f95f7d58d92854
tmuks <mukund.topiwala@gmail.com>
2018-05-09 23:56:43 +0530

    Task 7

---
+++ task7/Classes/InputFilter.php
@@ -0,0 +1,68 @@
+<?php
+class InputFilter {
+	/**
+	 * Input Values
+	 */
+	private $values = [];
+
+	/**
+	 * Input Values
+	 */
+	private $error = null;
+
+	/**
+	 * Max Input Values
+	 */
+	private $maxValue = 1000;
+
+	public function __construct(){
+
+	}
+
+	/**
+	 * Check if input argument are properly validated or not. If valid will be stored in $values.
+	 * @param string $arg Input data
+	 * @return boolean Returns true if valid else false.
+	 */
+	public function validate($arg){
+		$regex = '~^\d+(\,\d+)*$~';
+
+		try {
+
+			if(empty($arg)){
+				$this->values = [0];
+			} else if(preg_match($regex, $arg)){
+				$values = explode(',', $arg);
+
+				$this->values = $this->_getAllowedValues($values);
+			} else {
+				throw new Exception("\n========================================\nInvalid Input Data. Please try again! \n\nPossible input \\\\;\\\\4;5.	\n\n========================================\n");
+			}
+			return true;
+		}catch(Exception $e) {
+			$this->error = $e->getMessage();
+			return false;
+		}
+
+	}
+
+	/**
+	 * Get all the allowed values less than the max value
+	 */
+	private function _getAllowedValues($values){
+		$maxValue = $this->maxValue;
+		$allowedValues = array_filter($values, function ($v) use ($maxValue) {
+			return $v < $maxValue;
+		});
+		return $allowedValues;
+	}
+
+	public function getError(){
+		return $this->error;
+	}
+
+	public function getValues(){
+		return $this->values;
+	}
+
+}
---
+++ task7/README.md
@@ -0,0 +1,11 @@
+# Task7 Calculator
+
+## Usage
+`php task7\index.php add 10,20,1010,20`
+
+## Problem
+Ignore numbers above 1000. For example, command
+
+`php calculator.php add 10,20,1010,20`
+
+should return 50.
